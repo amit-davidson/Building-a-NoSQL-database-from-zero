f44846c690ea4008b68ee58234366bfada313e9e
Amit Davidson <sgtamit234@gmail.com>
2022-03-12 12:50:09 +0200

    Add page read and write

--- dal.go
+++ dal.go
@@ -5,18 +5,26 @@ import (
 	"os"
 )
 
-type dal struct {
-	file *os.File
+type pgnum uint64
+
+type page struct {
+	num  pgnum
+	data []byte
 }
 
+type dal struct {
+	file     *os.File
+	pageSize int
+}
 
-func newDal(path string) (*dal, error) {
+func newDal(path string, pageSize int) (*dal, error) {
 	dal := &dal{}
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
 	}
 	dal.file = file
+	dal.pageSize = pageSize
 	return dal, nil
 }
 
@@ -30,4 +38,27 @@ func (d *dal) close() error {
 	}
 
 	return nil
+}
+
+func (d *dal) allocateEmptyPage() *page {
+	return &page{
+		data: make([]byte, d.pageSize),
+	}
+}
+
+func (d *dal) readPage(pageNum pgnum) (*page, error) {
+	p := d.allocateEmptyPage()
+
+	offset := int(pageNum) * d.pageSize
+	_, err := d.file.ReadAt(p.data, int64(offset))
+	if err != nil {
+		return nil, err
+	}
+	return p, err
+}
+
+func (d *dal) writePage(p *page) error {
+	offset := int64(p.num) * int64(d.pageSize)
+	_, err := d.file.WriteAt(p.data, offset)
+	return err
 }
