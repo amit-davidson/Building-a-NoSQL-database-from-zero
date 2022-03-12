645ff1dde8044f29c3622e8449334cc82def7555
Amit Davidson <sgtamit234@gmail.com>
2022-03-12 21:34:40 +0200

    Add pageNumSize

--- freelist.go
+++ freelist.go
@@ -1,8 +1,11 @@
 package LibraDB
 
+import "encoding/binary"
+
 // metaPage is the maximum pgnum that is used by the db for its own purposes. For now, only page 0 is used as the
 // header page. It means all other page numbers can be used.
 const metaPage = 0
+const pageNumSize = 8
 
 // freelist manages the manages free and used pages.
 type freelist struct {
@@ -35,4 +38,38 @@ func (fr *freelist) getNextPage() pgnum {
 
 func (fr *freelist) releasePage(page pgnum) {
 	fr.releasedPages = append(fr.releasedPages, page)
+}
+
+func (fr *freelist) serialize() []byte {
+	buf := make([]byte, 4+pageNumSize*len(fr.releasedPages))
+	pos := 0
+
+	binary.LittleEndian.PutUint16(buf[pos:], uint16(fr.maxPage))
+	pos += 2
+
+	// released pages count
+	binary.LittleEndian.PutUint16(buf[pos:], uint16(len(fr.releasedPages)))
+	pos += 2
+
+	for _, page := range fr.releasedPages {
+		binary.LittleEndian.PutUint64(buf[pos:], uint64(page))
+		pos += pageNumSize
+
+	}
+	return buf
+}
+
+func (fr *freelist) deserialize(buf []byte) {
+	pos := 0
+	fr.maxPage = pgnum(binary.LittleEndian.Uint16(buf[pos:]))
+	pos += 2
+
+	// released pages count
+	releasedPagesCount := int(binary.LittleEndian.Uint16(buf[pos:]))
+	pos += 2
+
+	for i := 0; i < releasedPagesCount; i++ {
+		fr.releasedPages = append(fr.releasedPages, pgnum(binary.LittleEndian.Uint64(buf[pos:])))
+		pos += pageNumSize
+	}
 }
