821cc7635ef6f0ae4de73a4ded57bd19b7eabdba
Amit Davidson <sgtamit234@gmail.com>
2022-04-20 22:07:04 +0300

    Add part 3

---
+++ Part 3/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// initialize db
+	dal, _ := newDal("db.db")
+
+	// create a new page
+	p := dal.allocateEmptyPage()
+	p.num = dal.getNextPage()
+	copy(p.data[:], "data")
+
+	// commit it
+	_ = dal.writePage(p)
+	_, _ = dal.writeFreelist()
+
+	// Close the db
+	_ = dal.close()
+
+	// We expect the freelist state was saved, so we write to
+	// page number 3 and not overwrite the one at number 2
+	dal, _ = newDal("db.db")
+	p = dal.allocateEmptyPage()
+	p.num = dal.getNextPage()
+	copy(p.data[:], "data2")
+	_ = dal.writePage(p)
+}
---
+++ Part 3/meta.go
@@ -0,0 +1,29 @@
+package main
+
+import "encoding/binary"
+
+const (
+	metaPageNum = 0
+)
+
+// meta is the meta page of the db
+type meta struct {
+	freelistPage pgnum
+}
+
+func newEmptyMeta() *meta {
+	return &meta{}
+}
+
+func (m *meta) serialize(buf []byte) {
+	pos := 0
+
+	binary.LittleEndian.PutUint64(buf[pos:], uint64(m.freelistPage))
+	pos += pageNumSize
+}
+
+func (m *meta) deserialize(buf []byte) {
+	pos := 0
+	m.freelistPage = pgnum(binary.LittleEndian.Uint64(buf[pos:]))
+	pos += pageNumSize
+}
