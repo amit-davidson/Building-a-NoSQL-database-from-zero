fdc3905996c85427233ca15685a4bd7c8a4c59d1
Amit Davidson <sgtamit234@gmail.com>
2022-08-03 14:34:42 +0300

    Add main

--- Part 3/main.go
+++ Part 3/main.go
@@ -1,26 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	// initialize db
-	dal, _ := newDal("db.db")
+	dal, _ := newDal("./mainTest")
 
-	// create a new page
-	p := dal.allocateEmptyPage()
-	p.num = dal.getNextPage()
-	copy(p.data[:], "data")
+	node, _ := dal.getNode(dal.root)
+	node.dal = dal
+	index, containingNode, _ := node.findKey([]byte("Key1"))
+	res := containingNode.items[index]
 
-	// commit it
-	_ = dal.writePage(p)
-	_, _ = dal.writeFreelist()
-
-	// Close the db
+	fmt.Printf("key is: %s, value is: %s\n", res.key, res.value)
 	_ = dal.close()
-
-	// We expect the freelist state was saved, so we write to
-	// page number 3 and not overwrite the one at number 2
-	dal, _ = newDal("db.db")
-	p = dal.allocateEmptyPage()
-	p.num = dal.getNextPage()
-	copy(p.data[:], "data2")
-	_ = dal.writePage(p)
 }
