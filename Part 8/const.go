b0634748a4005aaa44a4d219351f5fe9ddfa20ab
Amit Davidson <sgtamit234@gmail.com>
2022-08-18 16:38:22 +0300

    Part 8

---
+++ Part 8/const.go
@@ -0,0 +1,14 @@
+package main
+
+import "errors"
+
+const (
+	magicNumberSize = 4
+	counterSize = 4
+	nodeHeaderSize = 3
+
+	collectionSize = 16
+	pageNumSize    = 8
+)
+
+var writeInsideReadTxErr = errors.New("can't perform a write operation inside a read transaction")
---
+++ Part 8/db.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"sync"
+)
+
+type DB struct {
+	rwlock   sync.RWMutex   // Allows only one writer at a time
+	*dal
+}
+
+func Open(path string, options *Options) (*DB, error) {
+	var err error
+
+	options.pageSize = os.Getpagesize()
+	dal, err := newDal(path, options)
+	if err != nil {
+		return nil, err
+	}
+
+	db := &DB{
+		sync.RWMutex{},
+		dal,
+	}
+
+	return db, nil
+}
+
+func (db *DB) Close() error {
+	return db.close()
+}
+
+func (db *DB) ReadTx() *tx {
+	db.rwlock.RLock()
+	return newTx(db, false)
+}
+
+func (db *DB) WriteTx() *tx {
+	db.rwlock.Lock()
+	return newTx(db, true)
+}
